5d2b937a42d14dbdb85a59d84762d2baee598fb6
DiscoloR <rmfield@qq.com>
2022-02-13 05:31:20 +0800

    增加了管理者对pixiv插件的控制功能

--- yuzuriha_inori_bot/plugins/pixiv/__init__.py
+++ yuzuriha_inori_bot/plugins/pixiv/__init__.py
@@ -1,18 +1,41 @@
 from nonebot import on_message
-from nonebot.adapters.cqhttp import MessageSegment, Message
-from nonebot.rule import startswith
+from nonebot.adapters.cqhttp import MessageSegment, Message, GroupMessageEvent, GroupRecallNoticeEvent
+from nonebot.permission import SUPERUSER
+from nonebot.rule import startswith, keyword
+from nonebot.adapters.onebot.v11 import Bot
 from .pixiv_ import get_picture
+
+status = True
+
+
+forbid = on_message(rule=keyword('可以涩涩', '不准涩涩'), permission=SUPERUSER)
+
+
+@forbid.handle()
+async def set_status(bot: Bot, event: GroupMessageEvent):
+    command = event.get_plaintext()
+    global status
+    if command == '可以涩涩':
+        status = True
+        await forbid.send('集,拜托了,请使用我吧!')
+    else:
+        status = False
+        await forbid.send('。。。')
 
 
 sese = on_message(rule=startswith('涩涩'), priority=10)
 
 
 @sese.handle()
-async def handle_func():
-    p = await get_picture()
-    if not p:
-        return False
-    sese.block = True
-    url = f'https://www.pixiv.net/artworks/{p.id}'
-    msg: Message = MessageSegment.text(url) + MessageSegment.image(file=p.pic)
-    await sese.finish(msg)
+async def send_picture(bot: Bot, event: GroupMessageEvent):
+    global status, msg_list
+    if status:
+        p = await get_picture()
+        if not p:
+            return False
+        sese.block = True
+        url = f'https://www.pixiv.net/artworks/{p.id}'
+        msg: Message = MessageSegment.text(url) + MessageSegment.image(file=p.pic)
+        await sese.finish(msg)
+    else:
+        await sese.send('不可以涩涩哦')
